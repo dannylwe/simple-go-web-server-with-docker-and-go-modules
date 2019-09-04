63d3b78f9a18d0e738d7467d502809aaacf76369
Daniel Lwetabe <daniel.lwetabe@andela.com>
2019-09-04 17:12:06 +0300

    add logrus for logging

--- server/main.go
+++ server/main.go
@@ -3,11 +3,13 @@ package main
 import (
 	"fmt"
 	"net/http"
+	log "github.com/sirupsen/logrus"
 )
 
 //HelloWeb function returns a welcome message
 func HelloWeb(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "welcome to Go")
+	log.Info("success, 200")
 }
 
 func main() {
